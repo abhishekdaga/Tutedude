892d3a8ebb5573233011990c07fdca14b1c03047
abhishekdaga <abhishek1986.nits@gmail.com>
2025-12-30 10:58:41 +0530

    Add basic arithmetic operations in task1.py
    Implement basic arithmetic operations on two user inputs.

---
+++ python/assignment1/task1.py
@@ -0,0 +1,24 @@
+"""
+Take in input from users, two numbers.
+perform addition, subtraction, multiplication, division on those two numbers.
+"""
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+# for addition
+sum = first_number + second_number
+
+# for substraction
+difference = first_number - second_number
+
+# for multiplication
+product = first_number * second_number
+
+# for division
+division = first_number / second_number
+
+print("Addition:", sum)
+print("Subtraction:", difference)
+print("Product:", product)
+print("Division:", division)
+
