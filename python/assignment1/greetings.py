cf6407dbf31a235d76f15c006f3141539dc1f358
abhishekdaga <abhishek1986.nits@gmail.com>
2025-12-30 11:43:43 +0530

    Add greeting script for user input

---
+++ python/assignment1/greetings.py
@@ -0,0 +1,8 @@
+"""
+Take in user's first name and last name as input and greet them.
+"""
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name+"! Welcome to the Python program.")
